777fb98158e8cccdcd2d475ead161af8744c8711
Jack Doran <jcdoran33@gmail.com>
2022-08-08 14:25:30 -0400

    created placeholder.js, export handler function

--- functions/placeholder.js
+++ functions/placeholder.js
@@ -1,6 +1,12 @@
 exports.handler = async (event, context) => {
+    const { path } = event;
+    const paramsPath = path.replace('/.netlify/functions/placeholder', '');
+    const [width, height, background] = paramsPath.split('/').filter(param => !!param);
+    console.log("width: ", width);
+    console.log("height: ", height);
+    console.log("background: ", background);
     return {
         statusCode: 200,
         body: JSON.stringify({status: 'Ok'})
     }
-}
+};
